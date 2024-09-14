96c26020cd38fdf94ca39b70d8e6448c74459f29
surya4206 <suryabhat.20037@gmail.com>
2024-09-14 20:54:22 +0530

    Added YUV to RGB conversion

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,39 +15,38 @@ def predict():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
     nparr = np.frombuffer(image_data, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-    #image = io.BytesIO(nparr)
-    #image = Image.open(image)
-    #image = np.array(image)
-    print(type(image))
-    # image_rgb = cv2.cvtColor(nparr, cv2.COLOR_BGR2RGB)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite('new_image.jpg', image)
+    label = {0:'cha', 1:'chha', 2:'da', 3:'dda', 4:'ddha', 5:'dha', 6:'ga', 7:'gha', 8:'ja', 9:'jha', 10:'ka', 11:'kha', 12:'na1', 13:'na2', 14:'na3', 15:'na4', 16:'ta', 17:'tha', 18:'tta', 19:'ttha'}
+
     
-    # # Process the image and find hands
-    # results = hands.process(nparr)
-    # if results.multi_hand_landmarks:
-    #     keypoints = []
-    #     for hand_landmarks in results.multi_hand_landmarks:
-    #         # Extract x and y coordinates of the landmarks
-    #         for landmark in hand_landmarks.landmark:
-    #             keypoints.extend([landmark.x, landmark.y])
+    #Process the image and find hands
+    results = hands.process(image)
+    print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        keypoints = []
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Extract x and y coordinates of the landmarks
+            for landmark in hand_landmarks.landmark:
+                keypoints.extend([landmark.x, landmark.y])
         
-    #     # Check the number of keypoints extracted
-    #     if len(keypoints) == 42:
-    #         # Only one hand detected, pad the remaining 42 keypoints with zeros
-    #         pass
-    #     else:
-    #         print("Unexpected number of keypoints detected.")
+        # Check the number of keypoints extracted
+        if len(keypoints) == 42:
+            # Only one hand detected, pad the remaining 42 keypoints with zeros
+            pass
+        else:
+            print("Unexpected number of keypoints detected.")
         
-    #     # Ensure keypoints are of the correct shape [1, 1, 84]
-    #     keypoints = np.array(keypoints).reshape(1, 1, 42)
+        # Ensure keypoints are of the correct shape [1, 1, 84]
+        keypoints = np.array(keypoints).reshape(1, 1, 42)
         
-    #     # Predict the label
-    #     prediction = model.predict(keypoints)
-    #     predicted_label = np.argmax(prediction, axis=1)
-    #     predicted_class = label_encoder.inverse_transform(predicted_label)[0]
-    
-    return jsonify({'prediction': 0})
+        # Predict the label
+        prediction = model.predict(keypoints)
+        predicted_label = np.argmax(prediction, axis=1)
+        print(predicted_label)
+        return jsonify({'prediction': label[predicted_label]})
+    return jsonify({'prediction': 'None'})
 
 if __name__ == '__main__':
-    model = load_model('mediapipe_lstm_model.h5')
+    model = load_model('./mediapipe_lstm_model.h5')
     app.run(host='0.0.0.0', port=5000)
--- sign_language_project/lib/pages/sign_language_recognition.dart
+++ sign_language_project/lib/pages/sign_language_recognition.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'dart:isolate';
 import 'dart:convert';
+import 'package:image/image.dart' as img;
 import 'package:http/http.dart' as http;
 
 class SignLanguageRecognitionPage extends StatefulWidget {
@@ -26,11 +27,12 @@ class _SignLanguageRecognitionPageState extends State<SignLanguageRecognitionPag
     _initializeCamera(_selectedCameraIndex);
   }
 
+
   Future<void> _initializeCamera(int cameraIndex) async {
     try {
       _cameras = await availableCameras();
       if (_cameras!.isNotEmpty) {
-        _cameraController = CameraController(_cameras![cameraIndex], ResolutionPreset.medium);
+        _cameraController = CameraController(_cameras![cameraIndex], ResolutionPreset.medium, imageFormatGroup: ImageFormatGroup.bgra8888);
         await _cameraController!.initialize();
         setState(() {
           _isCameraInitialized = true;
@@ -77,20 +79,22 @@ class _SignLanguageRecognitionPageState extends State<SignLanguageRecognitionPag
   static Future<String> _sendFrameToServer(CameraImage image) async {
     try {
       // Convert image to byte array
-      List<int> imageBytes = [];
-      for (Plane plane in image.planes) {
-        imageBytes.addAll(plane.bytes);
-      }
-
+           
+      // Step 2: Encode the RGB image data (e.g., to PNG)
+      List<int>? pngBytes = _convertYUVtoRGB(image);
+      if (pngBytes == null) {
+        return 'Error: Failed to convert image';
+      } 
       // Send POST request to Flask server
       var response = await http.post(
         Uri.parse('http://192.168.0.106:5000/predict'),  // Use 10.0.2.2 for Android emulator, localhost for iOS
         headers: {'Content-Type': 'application/octet-stream'},
-        body: imageBytes,
+        body: pngBytes,
       );
 
       if (response.statusCode == 200) {
         Map<String, dynamic> data = json.decode(response.body);
+        print(data['prediction']);
         return data['prediction'];
       } else {
         return 'Error: ${response.statusCode}';
@@ -99,6 +103,44 @@ class _SignLanguageRecognitionPageState extends State<SignLanguageRecognitionPag
       return 'Error: $e';
     }
   }
+
+  static List<int>? _convertYUVtoRGB(CameraImage image) {
+  try {
+    final int width = image.width;
+    final int height = image.height;
+    final int uvRowStride = image.planes[1].bytesPerRow;
+    final int uvPixelStride = image.planes[1].bytesPerPixel!;
+
+    var rgb = img.Image(height:height, width:width);
+
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        final int uvIndex = uvPixelStride * (x / 2).floor() + uvRowStride * (y / 2).floor();
+        final int index = y * width + x;
+
+        final yp = image.planes[0].bytes[index];
+        final up = image.planes[1].bytes[uvIndex];
+        final vp = image.planes[2].bytes[uvIndex];
+
+        int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+        int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91).round().clamp(0, 255);
+        int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+        const shift = (0xFF << 24);
+        if (rgb.isBoundsSafe(height-y, x)){ 
+              rgb.setPixelRgba(height-y, x, r , g ,b ,shift); 
+        } 
+          }
+        }
+
+        img.PngEncoder pngEncoder = new img.PngEncoder(level: 0);
+        List<int> png = pngEncoder.encode(rgb);
+        return png;  
+      } catch (e) {
+        print(">>>>>>>>>>>> ERROR:" + e.toString());
+      }
+      return null;
+
+}
 
   void _switchCamera() {
     if (_cameras != null && _cameras!.length > 1) {
@@ -109,6 +151,8 @@ class _SignLanguageRecognitionPageState extends State<SignLanguageRecognitionPag
       });
     }
   }
+
+  
 
   @override
   void dispose() {
